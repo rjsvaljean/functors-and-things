fb49425b2f8a538576ac484d1edeedfeb0623af4
Ratan Sebastian <ratan.sebastian@timgroup.com>
2013-03-02 18:12:38 -0500

    Put in tests so that the functor laws are exhibited somewhere

--- src/test/scala/rsebastian/FunctorSpec.scala
+++ src/test/scala/rsebastian/FunctorSpec.scala
@@ -7,11 +7,19 @@ class FunctorSpec extends Specification with ScalaCheck { def is =
   "FizzBuzz works as expected" ! {
     import rsebastian.Functor.ListFunctor
     fizzBuzz((1 to 10).toList) must_== List("1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz")
-  }
+  } ^
+  "The List functor obeys the functor laws" ^
+    "if we map the id function over a functor, the functor that we get back should be the same as the original functor" ! {
+      (functorOf[List].fmap[Nothing, Nothing](identity[Nothing])(List()) must_== List()) and
+        (functorOf[List].fmap(identity[Int])(List(1, 2, 3)) must_== List(1, 2, 3))
+    } ^
+    """composing two functions and then mapping the resulting function over a functor
+      |should be the same as first mapping one function over the functor and then mapping the other one.""".stripMargin ! {
+      val toStr = {(i: Int) => i.toString}
+      val appendThing = {(i: String) => i + " thing"}
+      (functorOf[List].fmap(toStr andThen appendThing)(List(1, 2)) must_== functorOf[List].fmap(appendThing)(functorOf[List].fmap(toStr)(List(1, 2))))
+    }
+  end
 
-//  ! prop { (a: Int) =>
-//    import rsebastian.Functor._
-//    fizzBuzz((((a * 10) + 1) to ((a + 1) * 10)).toList).count(_ == "Buzz") == 2
-//  }
-
+  def functorOf[F[_]](implicit f: Functor[F]) = f
 }
