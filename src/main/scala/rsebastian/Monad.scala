0965022d7008f570899978775d47cf977b9db1b5
Ratan Sebastian <ratan.sebastian@timgroup.com>
2013-03-09 22:57:22 -0500

    can not use the for comprehension with Monad

--- src/main/scala/rsebastian/Monad.scala
+++ src/main/scala/rsebastian/Monad.scala
@@ -8,8 +8,6 @@ trait Monad[F[_]] extends ApplicativeFunctor[F] {
   }
 }
 
-case class State[S, +A](transition: S => (S, A))
-
 object Monad {
   implicit def StateMonad[S] = new Monad[({type λ[α] = State[S, α]})#λ] {
     def flatMap[A, B](f: (A) => State[S, B]): (State[S, A]) => State[S, B] = { (s: State[S, A]) =>
@@ -17,10 +15,16 @@ object Monad {
         val (newState, a) = s.transition(s1)
         f(a).transition(newState)
       })
-
     }
 
     def pure[A]: (A) => State[S, A] = { (a: A) => State({(s: S) => (s, a)}) }
   }
 
+  case class State[S, A](transition: S => (S, A)) extends MonadicSyntax[A, ({type λ[α] = State[S, α]})#λ]
+}
+
+abstract class MonadicSyntax[A, M[_] : Monad] { self: M[A] =>
+  val monadOps: Monad[M] = implicitly[Monad[M]]
+  def flatMap[B](f: A => M[B]) = monadOps.flatMap(f)(self)
+  def map[B](f: A => B) = monadOps.fmap(f)(self)
 }
--- src/test/scala/rsebastian/MonadSpec.scala
+++ src/test/scala/rsebastian/MonadSpec.scala
@@ -1,13 +1,19 @@
 package rsebastian
 
 import org.specs2.Specification
+import Monad._
 
 class MonadSpec extends Specification { def is =
   "State monad" ^
     "accumulates state changing computations" ! {
-      def add(i: Int) = State((j: Int) => (j + i, ()))
-      val changeStateBy1 = add(1)
-      val changeStateBy2 = Monad.StateMonad[Int].flatMap[Unit, Unit](_ => add(2))(changeStateBy1)
-      changeStateBy2.transition(0)._1 must_== 3
+
+      val count = (s: String) => State((n: Int) => (n+1, s + n))
+
+      ((count("a-") flatMap count flatMap count).transition(0) must_== (3, "a-012")) and
+      ((for {
+        st1 <- count("a-")
+        st2 <- count(st1)
+        st3 <- count(st2)
+      } yield st3).transition(0) must_== (3, "a-012"))
     }
 }
