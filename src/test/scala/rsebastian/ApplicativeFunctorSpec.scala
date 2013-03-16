fe8b01013f1d4249d428d567fbd65db9e86d065a
Ratan Sebastian <ratan.sebastian@timgroup.com>
2013-03-16 11:03:25 -0400

    Added applicative laws

--- src/test/scala/rsebastian/ApplicativeFunctorSpec.scala
+++ src/test/scala/rsebastian/ApplicativeFunctorSpec.scala
@@ -3,12 +3,40 @@ package rsebastian
 import org.specs2.Specification
 import rsebastian.OptionHelpers._
 import rsebastian.WhyApplicativeFunctor.{sequence, validate}
+import rsebastian.ApplicativeFunctor._
 import org.specs2.matcher.DataTables
-import rsebastian.Monad.State
-import rsebastian.Functor.ListFunctor
-import rsebastian.ApplicativeFunctor.{OptionApplicativeFunctor, ListApplicativeFunctor}
 
-class ApplicativeFunctorSpec extends Specification with DataTables { def is =
+
+class ApplicativeFunctorSpec extends Specification with DataTables {
+  implicit def ValidationApp = new ValidationApplicativeFunctor[Int]()
+  def pureId[A] = IdentApplicativeFunctor.pure[A]
+  def appId[A, B] = IdentApplicativeFunctor.apply[A, B] _
+  def pureOpt[A] = OptionApplicativeFunctor.pure[A]
+  def appOpt[A, B] = OptionApplicativeFunctor.apply[A, B] _
+  def pureVal[A] = ValidationApp.pure[A]
+  def appVal[A, B] = ValidationApp.apply[A, B] _
+  def toInt: Double => Int = _.toInt
+  def toDouble: String => Double = _.toDouble
+
+  def is =
+  "Applicative laws: " ^
+    "Identity" ! {
+      (appId(pureId(identity[Int] _))(pureId(1)) must_== pureId(1)) and
+      (appOpt(pureOpt(identity[Int] _))(pureOpt(1)) must_== pureOpt(1)) and
+      (appVal(pureVal(identity[Int] _))(pureVal(1)) must_== pureVal(1))
+    } ^
+    "Composition" ! {
+      appOpt(pureOpt(toInt))(appOpt(pureOpt(toDouble))(pureOpt("1.1"))).must_==(
+        appOpt(pureOpt(toDouble andThen toInt))(pureOpt("1.1"))
+      )
+    } ^
+    "Homomorphism" ! {
+      appVal(pureVal(toInt))(pureVal(1.1)) must_== pureVal(toInt(1.1))
+    } ^
+    "Interchange" ! {
+      val fab: Option[(Double) => Int] = pureOpt(toInt)
+      appOpt(fab)(pureOpt(1.1)) must_== appOpt(pureOpt((f: Double => Int) => f(1.1)))(fab)
+    } ^ bt ^
   "sequence should work as expected" ^
     "for a list of Options" ! {
       (sequence(List[Option[Nothing]]()) must_== just(Nil)) and
